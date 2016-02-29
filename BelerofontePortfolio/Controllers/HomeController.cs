4b37481a572127963f0994a37ac998245e6d896e
Shingo Yabuki <bryanprsvar@gmail.com>
2016-02-28 23:22:40 -0300

    estructura MVC  y creacion DB

---
+++ BelerofontePortfolio/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using Model;
+
+namespace BelerofontePortfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        // localhost8086/home
+        public ActionResult Index()
+        {
+         //   ViewBag.Alumnos = Alumno.Listar();
+            return View();
+        }
+
+        public ActionResult Ver()
+        {           
+            return View(Alumno.Obtener());
+        }
+
+        public ActionResult Guardar(Alumno a, int[] algo)
+        {
+            return Redirect("~/home/index");
+        }
+    }
+}
---
+++ Model/Alumno.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Alumno
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+
+        public static List<Alumno> Listar()
+        {
+            var alumnos = new List<Alumno>();
+            for (int i=0; i<=10; i++)
+            {
+                alumnos.Add(new Alumno() {
+                    ID = i,
+                    Nombre = "Alumno " + i
+                });
+            }
+            return alumnos;
+        }
+
+        public static Alumno Obtener()
+        {
+            return new Alumno()
+            {
+                ID = 1,
+                Nombre = "Alumno 1"
+            };
+        }
+    }
+}
